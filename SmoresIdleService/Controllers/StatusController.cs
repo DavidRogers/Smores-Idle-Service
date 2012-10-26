59c833d50986a36800466aa892a6ceee81696195
David Rogers <david.rogers@logos.com>
2012-10-26 15:23:45 -0700

    initial commit of idle code

---
+++ SmoresIdleService/Controllers/StatusController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace SmoresIdleService.Controllers
+{
+	public class StatusController : ApiController
+	{
+		[HttpGet]
+		public int UniqueUser(string id)
+		{
+			using (UserStatusDataContext context = new UserStatusDataContext())
+			{
+				UserStatus user = context.UserStatus.FirstOrDefault(x => x.UserHash == id);
+				return user == null ? -1 : user.Status;
+			}
+		}
+
+		[HttpPost]
+		public void UniqueUser(string id, int status)
+		{
+			using (UserStatusDataContext context = new UserStatusDataContext())
+			{
+				UserStatus user = context.UserStatus.FirstOrDefault(x => x.UserHash == id);
+				if (user != null)
+					user.Status = status;
+				else
+					context.UserStatus.InsertOnSubmit(new UserStatus { UserHash = id, Status = status });
+
+				context.SubmitChanges();
+			}
+
+		}
+	}
+}
