201f8aa8e9599cf0f5d4f991ea95360cf3d9832b
David Rogers <david.rogers@logos.com>
2012-10-26 20:10:23 -0700

    using json models

--- SmoresIdleService/Controllers/StatusController.cs
+++ SmoresIdleService/Controllers/StatusController.cs
@@ -15,34 +15,47 @@ namespace SmoresIdleService.Controllers
 
 	public class StatusController : ApiController
 	{
-		public int Post(string id)
+		public UserStatus Post(UserStatus userStatus)
 		{
 			string uriString = ConfigurationManager.AppSettings["SQLSERVER_CONNECTION_STRING"];
 			using (UserStatusDataContext context = new UserStatusDataContext(uriString))
 			{
-				UserStatus user = context.UserStatus.FirstOrDefault(x => x.UserHash == id);
-				return user == null ? (int) UserStatusEnum.Unknown : user.Status;
+				UserStatus user = context.UserStatus.FirstOrDefault(x => x.UserHash == userStatus.UserHash);
+				return new UserStatus
+				{
+					Status = user == null ? (int) UserStatusEnum.Unknown : user.Status,
+					LastUpdated = user == null ? DateTime.UtcNow : user.LastUpdated,
+					UserHash = userStatus.UserHash
+				};
 			}
 		}
 
-		public void Put(string id, int status)
+		public UserStatus Put(UserStatus userStatus)
 		{
 			string uriString = ConfigurationManager.AppSettings["SQLSERVER_CONNECTION_STRING"];
+			UserStatus user;
 			using (UserStatusDataContext context = new UserStatusDataContext(uriString))
 			{
-				UserStatus user = context.UserStatus.FirstOrDefault(x => x.UserHash == id);
+				user = context.UserStatus.FirstOrDefault(x => x.UserHash == userStatus.UserHash);
 				if (user != null)
 				{
-					user.Status = status;
+					user.Status = userStatus.Status;
 					user.LastUpdated = DateTime.UtcNow;
 				}
 				else
 				{
-					context.UserStatus.InsertOnSubmit(new UserStatus { UserHash = id, Status = status, LastUpdated = DateTime.UtcNow });
+					context.UserStatus.InsertOnSubmit(user = new UserStatus { UserHash = userStatus.UserHash, Status = userStatus.Status, LastUpdated = DateTime.UtcNow });
 				}
 
 				context.SubmitChanges();
 			}
+
+			return new UserStatus
+			{
+				LastUpdated = user.LastUpdated,
+				Status = user.Status,
+				UserHash = user.UserHash
+			};
 		}
 	}
 }
