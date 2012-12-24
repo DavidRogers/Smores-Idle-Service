b3f5d741cf002b8ebfeb75940b6f3929f0f71829
David Rogers <david.rogers@logos.com>
2012-12-24 15:01:00 -0800

    handle no conneciton in health check

--- SmoresIdleService/Controllers/HomeController.cs
+++ SmoresIdleService/Controllers/HomeController.cs
@@ -24,18 +24,22 @@ namespace SmoresIdleService.Controllers
 				if (hub != null)
 				{
 					int connections = hub.Subscriptions.UserSubscriptions.Count;
-					double average = hub.Subscriptions.UserSubscriptions.Average(x => x.Value.Count);
-					int floor = hub.Subscriptions.UserSubscriptions.Min(x => x.Value.Count);
-					int ceiling = hub.Subscriptions.UserSubscriptions.Max(x => x.Value.Count);
+					if (connections > 0)
+					{
+						double average = hub.Subscriptions.UserSubscriptions.Average(x => x.Value.Count);
+						int floor = hub.Subscriptions.UserSubscriptions.Min(x => x.Value.Count);
+						int ceiling = hub.Subscriptions.UserSubscriptions.Max(x => x.Value.Count);
 
-					StringBuilder healthContent = new StringBuilder();
-					healthContent.AppendLine("Connection Health");
-					healthContent.AppendLine(string.Format("{0} total connections", connections));
-					healthContent.AppendLine(string.Format("{0} average subscriptions per connection", average));
-					healthContent.AppendLine(string.Format("{0} subscriptions is the least", floor));
-					healthContent.AppendLine(string.Format("{0} subscriptions is the most", ceiling));
+						StringBuilder healthContent = new StringBuilder();
+						healthContent.AppendLine("Connection Health");
+						healthContent.AppendLine(string.Format("{0} total connections", connections));
+						healthContent.AppendLine(string.Format("{0} average subscriptions per connection", average));
+						healthContent.AppendLine(string.Format("{0} subscriptions is the least", floor));
+						healthContent.AppendLine(string.Format("{0} subscriptions is the most", ceiling));
 
-					return new ContentResult { Content = healthContent.ToString(), ContentType = "text/plain" };
+						return new ContentResult { Content = healthContent.ToString(), ContentType = "text/plain" };
+					}
+					return new ContentResult { Content = "No connections", ContentType = "text/plain" };
 				}
 				return new ContentResult { Content = "Could not resolve hub", ContentType = "text/plain" };
 			}
