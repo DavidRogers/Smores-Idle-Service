722874b8a89d324a9200663a4719b99e2b5c06ab
David Rogers <david.rogers@logos.com>
2012-12-23 19:44:15 -0800

    fix bug in hub health check

--- SmoresIdleService/Controllers/HomeController.cs
+++ SmoresIdleService/Controllers/HomeController.cs
@@ -17,21 +17,24 @@ namespace SmoresIdleService.Controllers
 		public ActionResult HubHealthCheck()
 		{
 			DefaultHubManager hd = new DefaultHubManager(GlobalHost.DependencyResolver);
-			StatusHub hub = (StatusHub) hd.ResolveHub("status");
+			StatusHub hub = hd.ResolveHub("status") as StatusHub;
+			if (hub != null)
+			{
+				int connections = hub.Subscriptions.UserSubscriptions.Count;
+				double average = hub.Subscriptions.UserSubscriptions.Average(x => x.Value.Count);
+				int floor = hub.Subscriptions.UserSubscriptions.Min(x => x.Value.Count);
+				int ceiling = hub.Subscriptions.UserSubscriptions.Max(x => x.Value.Count);
 
-			int connections = hub.Subscriptions.UserSubscriptions.Count;
-			double average = hub.Subscriptions.UserSubscriptions.Average(x => x.Value.Count);
-			int floor = hub.Subscriptions.UserSubscriptions.Min(x => x.Value.Count);
-			int ceiling = hub.Subscriptions.UserSubscriptions.Max(x => x.Value.Count);
+				StringBuilder healthContent = new StringBuilder();
+				healthContent.AppendLine("Connection Health");
+				healthContent.AppendLine(string.Format("{0} total connections", connections));
+				healthContent.AppendLine(string.Format("{0} average subscriptions per connection", average));
+				healthContent.AppendLine(string.Format("{0} subscriptions is the least", floor));
+				healthContent.AppendLine(string.Format("{0} subscriptions is the most", ceiling));
 
-			StringBuilder healthContent = new StringBuilder();
-			healthContent.AppendLine("Connection Health");
-			healthContent.AppendLine(string.Format("{0} total connections", connections));
-			healthContent.AppendLine(string.Format("{0} average subscriptions per connection", average));
-			healthContent.AppendLine(string.Format("{0} subscriptions is the least", floor));
-			healthContent.AppendLine(string.Format("{0} subscriptions is the most", ceiling));
-
-			return new ContentResult { Content = healthContent.ToString(), ContentType = "text/plain" };
+				return new ContentResult { Content = healthContent.ToString(), ContentType = "text/plain" };
+			}
+			return new ContentResult { Content = "Could not resolve hub", ContentType = "text/plain" };
 		}
 	}
 }
