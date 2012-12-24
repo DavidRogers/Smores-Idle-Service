1ef5e38ea2c22df03cb1bd178cf11ddb95ce4229
David Rogers <david.rogers@logos.com>
2012-12-23 19:53:08 -0800

    add some error logging for new hub health check

--- SmoresIdleService/Controllers/HomeController.cs
+++ SmoresIdleService/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Text;
 using System.Web.Mvc;
 using Microsoft.AspNet.SignalR;
@@ -16,25 +17,33 @@ namespace SmoresIdleService.Controllers
 
 		public ActionResult HubHealthCheck()
 		{
-			DefaultHubManager hd = new DefaultHubManager(GlobalHost.DependencyResolver);
-			StatusHub hub = hd.ResolveHub("status") as StatusHub;
-			if (hub != null)
+			try
 			{
-				int connections = hub.Subscriptions.UserSubscriptions.Count;
-				double average = hub.Subscriptions.UserSubscriptions.Average(x => x.Value.Count);
-				int floor = hub.Subscriptions.UserSubscriptions.Min(x => x.Value.Count);
-				int ceiling = hub.Subscriptions.UserSubscriptions.Max(x => x.Value.Count);
+				DefaultHubManager hd = new DefaultHubManager(GlobalHost.DependencyResolver);
+				StatusHub hub = hd.ResolveHub("status") as StatusHub;
+				if (hub != null)
+				{
+					int connections = hub.Subscriptions.UserSubscriptions.Count;
+					double average = hub.Subscriptions.UserSubscriptions.Average(x => x.Value.Count);
+					int floor = hub.Subscriptions.UserSubscriptions.Min(x => x.Value.Count);
+					int ceiling = hub.Subscriptions.UserSubscriptions.Max(x => x.Value.Count);
 
-				StringBuilder healthContent = new StringBuilder();
-				healthContent.AppendLine("Connection Health");
-				healthContent.AppendLine(string.Format("{0} total connections", connections));
-				healthContent.AppendLine(string.Format("{0} average subscriptions per connection", average));
-				healthContent.AppendLine(string.Format("{0} subscriptions is the least", floor));
-				healthContent.AppendLine(string.Format("{0} subscriptions is the most", ceiling));
+					StringBuilder healthContent = new StringBuilder();
+					healthContent.AppendLine("Connection Health");
+					healthContent.AppendLine(string.Format("{0} total connections", connections));
+					healthContent.AppendLine(string.Format("{0} average subscriptions per connection", average));
+					healthContent.AppendLine(string.Format("{0} subscriptions is the least", floor));
+					healthContent.AppendLine(string.Format("{0} subscriptions is the most", ceiling));
 
-				return new ContentResult { Content = healthContent.ToString(), ContentType = "text/plain" };
+					return new ContentResult { Content = healthContent.ToString(), ContentType = "text/plain" };
+				}
+				return new ContentResult { Content = "Could not resolve hub", ContentType = "text/plain" };
 			}
-			return new ContentResult { Content = "Could not resolve hub", ContentType = "text/plain" };
+			catch (Exception e)
+			{
+				return new ContentResult { Content = "Fail! " + e.Message + "\r\n" + e.StackTrace, ContentType = "text/plain" };
+			}
+
 		}
 	}
 }
